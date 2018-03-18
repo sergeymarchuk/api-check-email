0b47cc9986e65b65d7fab4ffeb5eb532e9f91c58
Sergey Marchuk <marchuk1s1p@gmail.com>
2018-03-18 15:18:43 +0200

    put user form in single file

---
+++ send_form.php
@@ -0,0 +1,7 @@
+<form class='form-signin mt-5' action='index.php' method='post' style="width: 30%; min-width: 350px; margin: auto;">
+    <h1 class="h3 mb-3 font-weight-normal">Pleae entry email and text</h1>
+    <input class="mb-3 btn-block" type='text' placeholder='email' name='email'>
+    <input class="mb-3 btn-block" type='textarea' placeholder='text' name='text'>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Send email</button>
+    <p class="mt-5 mb-3 text-muted">&copy MARICH 2018</p>
+</form>
